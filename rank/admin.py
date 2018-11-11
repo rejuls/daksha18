fa745b42fdc05c8c51bc2fe6764125a12a090a27
RejulS <s.rejul10@gmail.com>
2018-11-11 12:04:09 +0530

    First commit

---
+++ rank/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from rank.models import rank
+
+
+# Register your models here.
+admin.site.register(rank)
---
+++ results/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from results.models import event
+
+# Register your models here.
+admin.site.register(event)
