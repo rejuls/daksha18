85045be87277eefe3944be752b4b946040c1e39c
RejulS <s.rejul10@gmail.com>
2018-11-12 19:39:00 +0530

    directory change

--- daksha/urls.py
+++ daksha/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rank.urls')),
+	url('', include('rank.urls')),
     path('results/',include('results.urls')),
 ]
