748be89fba81007640821e752741e71ca8ad11f7
vishnu-vk <33258967+vishnu-vk@users.noreply.github.com>
2018-11-13 20:22:57 +0530

    Add files via upload

--- rank/templates/js/main.js
+++ rank/templates/js/main.js
@@ -1,44 +1,29 @@
 // Responsive Nav
-$(function() {
-  menu = $('nav ul');
+function countdown()
+{
+  var now=new Date();
+  var e=new Date(2018,10,21);
+  var cTime=now.getTime();
+  var eTime=e.getTime();
+  var d=eTime-cTime;
+  var s=Math.floor(d/1000);
+  var m=Math.floor(s/60);
+  var h=Math.floor(m/60);
+  var d=Math.floor(h/24);
+  h%=24;
+  m%=60;
+  s%=60;
+  h=(h<10) ? '0' + h : h;
+  m=(m<10) ? '0' + m : m;
+  s=(s<10) ? '0' + s : s;
 
-  $('#openup').on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
+  document.getElementById("days").innerText=d;
+  document.getElementById("hours").innerText=h;
+  document.getElementById("minutes").innerText=m;
+  document.getElementById("seconds").innerText=s;
+  setTimeout(countdown,1000);
+}
+$(document).ready(function(){
+countdown();
 
-  $(window).resize(function() {
-    var w = $(this).width();
-    if (w > 480 && menu.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-    
-  });
-
-  $('nav li').on('click', function(e) {
-    var w = $(window).width();
-    if (w < 480) {
-      menu.slideToggle();
-    }
-  });
-  $('.open-menu').height($(window).height());
-});
-
-// Smooth Scrolling
-$('.cf a').on('click', function(event) {
-  if (this.hash !== '') {
-    event.preventDefault();
-
-    const hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      800,
-      function() {
-        window.location.hash = hash;
-      }
-    );
-  }
 });
