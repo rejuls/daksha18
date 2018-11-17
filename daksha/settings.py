cec87c2726823711e3c1481d96309c6bbc434585
MargareV <hello.margrette@gmail.com>
2018-11-17 16:18:43 +0530

    chages to settimgs

--- daksha/settings.py
+++ daksha/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,6 +28,7 @@ SECRET_KEY = '@117&l1tptqxmr71j=2(iy80tpzpi*gk0orw1t!_q8+ck2^q^m'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
