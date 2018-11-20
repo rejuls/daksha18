859d02f4d37e50cd4d38cb918bd9a7b8cb8bca84
RejulS <s.rejul10@gmail.com>
2018-11-20 15:08:42 +0530

    reg closed

--- rank/urls.py
+++ rank/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	path('register/', views.reg_closed, name='register_page'),
 	path('events/',views.event, name='event'),
 	path('',views.index, name='index'),
-	path('export/csv/', views.export_users_csv, name='export_users_csv'),
+	#path('export/csv/', views.export_users_csv, name='export_users_csv'),
 	path('team/',views.team, name='team')
 ]
