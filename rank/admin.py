aaf401f6240e653ca6a4a5c67dfadcfc5bf83603
RejulS <s.rejul10@gmail.com>
2018-11-20 12:27:22 +0530

    clean

--- rank/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Point,Result,Registration
-
-
-# Register your models here.
-admin.site.register(Point)
-admin.site.register(Result)
-admin.site.register(Registration)
-
--- rank/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from . models import Registration
-
-class RegistrationForm(ModelForm):
-	class Meta:
-		model = Registration
-		fields = ['full_name','year','department','phone','events']
-		
