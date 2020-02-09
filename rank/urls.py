81e8a216aaa116c69b4e423ff8d4b9f97425db89
RejulS <s.rejul10@gmail.com>
2020-02-09 17:28:54 +0530

    Reboot for DAKSHA 2020

--- rank/urls.py
+++ rank/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	path('register/', views.reg_closed, name='register_page'),
-	path('events/',views.event, name='event'),
-	path('',views.index, name='index'),
+	#path('register/', views.reg_closed, name='register_page'),
+	#path('events/',views.event, name='event'),
+	#path('',views.index, name='index'),
 	#path('export/csv/', views.export_users_csv, name='export_users_csv'),
-	path('team/',views.team, name='team'),
-	path('results/',views.result_view,name='result')
+	#path('team/',views.team, name='team'),
+	path('',views.result_view,name='result')
 ]
